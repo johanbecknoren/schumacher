59f8f836c419117d4b57b5e9fab7a51dac0b243a
johbe559 <johan.noren@gmail.com>
2013-09-13 14:06:41 +0200

    Tog bort debug-prints

--- src/sphere.cpp
+++ src/sphere.cpp
@@ -20,18 +20,13 @@ IntersectionPoint *Sphere::getIntersectionPoint(Ray *ray) const {
 	glm::vec3 dir = glm::normalize(ray->getDirection());
 
 	if(d != NULL) {// Intersection with AABB exists
-		std::cout << "\nSphere.cpp - Found intersection with AABB\n";
 		A = glm::dot(dir, dir);
 		B = 2 * glm::dot((ray->getOrigin() - _position), dir);
 		C = glm::dot((ray->getOrigin() - _position), (ray->getOrigin() - _position)) - _radius*_radius;
 		
-		std::cout<<"A = "<<A<<", B = "<<B<<", C = "<<C<<"\n";
-
-		std::cout <<"1\n";
 		float sqrtTerm = B*B - 4*A*C;
 
 		if(sqrtTerm < 0.0f) {// Imaginary root(s)
-			std::cout << "Sphere.cpp - No intersection, imaginary roots\n";
 			return NULL;
 		}
 
@@ -52,7 +47,6 @@ IntersectionPoint *Sphere::getIntersectionPoint(Ray *ray) const {
 		}
 
 		if(t1 < 0.0f) {// Intersection, but in the ray's negative direction
-			std::cout << "Sphere.cpp -Intersection in ray's negative direction\n";
 			return NULL;
 		}
 
@@ -62,14 +56,12 @@ IntersectionPoint *Sphere::getIntersectionPoint(Ray *ray) const {
 			t = t0;
 
 		//ray->print();
-		std::cout<<"t = "<<t<<", t0 = "<<t0<<", t1 = "<<t1<<"\n";
 		glm::vec3 intP = ray->getOrigin() + glm::normalize(ray->getDirection())*t;
 		glm::vec3 surfNormal = glm::normalize(intP - _position);
 
    		return new IntersectionPoint(intP, surfNormal);	
   }
   else { // No intersection with sphere's AABB
-	std::cout << "Sphere.cpp - No intersection with AABB\n";
     return NULL;
   }
 }
