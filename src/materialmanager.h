2a27ad747f44d2a1f8796246d6c0fcb484be10b5
johbe559 <johan.noren@gmail.com>
2013-11-05 11:35:51 +0100

    Added files

---
+++ src/materialmanager.h
@@ -0,0 +1,14 @@
+#ifndef MATERIALMANAGER_H
+#define MATERIALMANAGER_H
+#include "material.h"
+#include "utils.h"
+#include <vector>
+
+class MaterialManager {
+public:
+	MaterialManager();
+	static Material* get(const material_t &m) { return MaterialManager::_materials.at(m); }
+
+	static std::vector<Material*> _materials;
+};
+#endif
